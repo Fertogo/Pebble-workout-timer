27a6166d900fcca571d0d1aeb429c4419336455b
Fernando Trujano <trujano@mit.edu>
2015-06-21 11:55:41 -0700

    Fix #57 Proper token migration 3.0

--- Online Workout Manger/save.php
+++ Online Workout Manger/save.php
@@ -1,9 +1,14 @@
 <?php
     echo("Starting PHP");
-    $token = $_GET["token"]; 
+    $token = $_GET["token"];
+    if (!ctype_alnum ($token)){
+        echo("Invalid Token");
+        return;
+    }
+
     $filename = "userworkouts/".$token.".json";
-    $file = fopen("$filename", "w"); 
-    fwrite($file, $_POST["data"]); 
-    fclose($file); 
+    $file = fopen("$filename", "w");
+    fwrite($file, $_POST["data"]);
+    fclose($file);
     echo("PHP done")
 ?>
