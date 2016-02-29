f460daf2a0a9fb58b37fb98630ad3871b2b0282d
Fernando Trujano <trujano.fernando@gmail.com>
2016-02-29 10:31:48 -0500

    #77 Write app.js specs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
+/* 
+Pebble JS component of Custom Workout Timer. v4.0
+Communicates with OWM, parses and stores workouts and handles workout requests from Pebble. 
+Copyright © 2016 Fernando Trujano
+                 trujano@mit.edu
+*/
+
 //--------- js-message-queue ----------//
 "use strict";(function(root,factory){if(typeof define==="function"&&define.amd){define([],factory)}else if(typeof module==="object"&&module.exports){module.exports=factory()}else{root.MessageQueue=factory()}})(this,function(){var RETRY_MAX=5;var queue=[];var sending=false;var timer=null;return{reset:reset,sendAppMessage:sendAppMessage,size:size};function reset(){queue=[];sending=false}function sendAppMessage(message,ack,nack){if(!isValidMessage(message)){return false}queue.push({message:message,ack:ack||null,nack:nack||null,attempts:0});setTimeout(function(){sendNextMessage()},1);return true}function size(){return queue.length}function isValidMessage(message){if(message!==Object(message)){return false}var keys=Object.keys(message);if(!keys.length){return false}for(var k=0;k<keys.length;k+=1){var validKey=/^[0-9a-zA-Z-_]*$/.test(keys[k]);if(!validKey){return false}var value=message[keys[k]];if(!validValue(value)){return false}}return true;function validValue(value){switch(typeof value){case"string":return true;case"number":return true;case"object":if(toString.call(value)==="[object Array]"){return true}}return false}}function sendNextMessage(){if(sending){return}var message=queue.shift();if(!message){return}message.attempts+=1;sending=true;Pebble.sendAppMessage(message.message,ack,nack);timer=setTimeout(function(){timeout()},1e3);function ack(){clearTimeout(timer);setTimeout(function(){sending=false;sendNextMessage()},200);if(message.ack){message.ack.apply(null,arguments)}}function nack(){clearTimeout(timer);if(message.attempts<RETRY_MAX){queue.unshift(message);setTimeout(function(){sending=false;sendNextMessage()},200*message.attempts)}else{if(message.nack){message.nack.apply(null,arguments)}}}function timeout(){setTimeout(function(){sending=false;sendNextMessage()},1e3);if(message.ack){message.ack.apply(null,arguments)}}}});
 
 var VERSION = "4.0";
 var MESSAGE_CHUNK_LENGTH = 100; 
 var MESSAGE_DELIMITER = "\t"; 
+var MESSAGE_DATA_ROOT = 2; 
 
 var DEV_MODE = false; //Set to false before Shipping! 
 var BASE_URL = 'http://pebble.fernandotrujano.com';
@@ -24,12 +32,23 @@ Pebble.addEventListener("showConfiguration", function(){
   Pebble.openURL(BASE_URL + "/user/home?info=" + Pebble.getAccountToken() + "," + VERSION);
 });
 
+/**
+* Assambles and sends a message to Pebble
+* @param: String: messageType: String representing the type of message
+* @param: String: messageHeader:  Delimiter separated message Header
+* @param: Object: message: Object with (key, value) pairs with integer keys starting at MESSAGE_DATA_ROOT
+*/
 function sendMessage(messageType, messageHeader, message) {
   message[0] = messageType; 
   message[1] = messageHeader; 
   console.log("sending message of type: " + messageType); 
   MessageQueue.sendAppMessage(message, function(e) { console.log("send yes"); }, function(e){console.log("ERROR: " + e.error.message); console.log("send no"); });
 }
+/**
+* Constructs a workout message and sends it to Pebble
+* @param String: WorkoutName: name of workout
+* @param Array: moves: Array of moves as encoded by OWM
+*/
 function sendWorkout(workoutName, moves) {
   console.log("sending workout" + workoutName + " with moves: " + moves); 
   
@@ -45,7 +64,7 @@ function sendWorkout(workoutName, moves) {
     
     chunk = moves.slice(i, i+MESSAGE_CHUNK_LENGTH);
     
-    messageIndex = 2; //Start moves at 2 
+    messageIndex = MESSAGE_DATA_ROOT; //Start moves at MESSAGE_DATA_ROOT to make space for type and header
     //Add each move in chunk to message
     for (j=0, len_j=chunk.length; j<len_j; j++) { 
       move = chunk[j]; 
@@ -59,13 +78,17 @@ function sendWorkout(workoutName, moves) {
   
 }
 
+/**
+* Parses workout, save them to localStorage and send titles to Pebble
+* @param String: jsonString: JSON string to parse 
+*/
 function parseAndAddWorkouts(jsonString) { 
   console.log("Configuration window returned: " + jsonString); //See repo for json structure
 
   var json; 
   json = JSON.parse(decodeURIComponent(jsonString));  
 
-  var workoutTitle,moves, workoutInfo, messageIndex=2, message={}; 
+  var workoutTitle,moves, workoutInfo, messageIndex=MESSAGE_DATA_ROOT, message={}; 
 
   localStorage.clear(); 
   //Add Each workout
@@ -83,7 +106,6 @@ function parseAndAddWorkouts(jsonString) {
   
   sendMessage("WORKOUTS", "NONE", message); 
     
-
 }
 
 
@@ -96,37 +118,45 @@ Pebble.addEventListener("webviewclosed",function(e) {
   }
 );
 
-//Workout requested from phone
-function workoutMessageHandler(workoutTitle) { 
-  console.log("Workout Requested: " + workoutTitle); 
-  var moves = localStorage.getItem(workoutTitle); 
+/**
+* Handles message requests from Pebble by finding and sending requested workout
+* @param String: workoutName: Name of requested workout
+*/
+function workoutMessageHandler(workoutName) { 
+  console.log("Workout Requested: " + workoutName); 
+  var moves = localStorage.getItem(workoutName); 
   console.log(moves); 
   moves = JSON.parse(moves); 
 
-  sendWorkout(workoutTitle, moves); 
+  sendWorkout(workoutName, moves); 
 }
 
-//TODO Call this from Pebble side
-function workoutDoneMessageHandler(workoutTitle) { 
-  workoutCompleted(workoutTitle); 
-
+/**
+* Handles workoutDone messages
+* @param String: workoutName: Name of recently completed workout
+*/
+function workoutDoneMessageHandler(workoutName) { 
+  workoutCompleted(workoutName); 
 }
 
-
-function workoutCompleted(title){ 
+/**
+* Logs completed workouts to OWM
+* @param String: workoutName: Name of recently completed workout
+*/
+function workoutCompleted(workoutName){ 
   console.log("sending request");
 
   if (Pebble.getActiveWatchInfo && Pebble.getActiveWatchInfo().firmware.major >= 3) { 
-    Pebble.getTimelineToken(function (timelineToken) {
+    Pebble.getTimelineToken(function(timelineToken) {
         var url = [ 
               BASE_URL,
               "/user/workout/completed/" , 
               encodeURIComponent(Pebble.getAccountToken()), "/",
-              encodeURIComponent(title), "/",
+              encodeURIComponent(workoutName), "/",
               timelineToken 
         ].join("");
         console.log("URL: " + url);
-        postWorkoutCompleted(url);
+        postRequest(url);
 
       },function (error) { 
         console.log('Error getting timeline token: ' + error);
@@ -135,14 +165,17 @@ function workoutCompleted(title){
   }
 
   else { 
-      var url = BASE_URL + '/user/workout/completed/' + encodeURIComponent(  Pebble.getAccountToken() )  + '/' + encodeURIComponent(localStorage.getItem("currentWorkoutName")) + "/none"; 
-      postWorkoutCompleted(url);
+      var url = BASE_URL + '/user/workout/completed/' + encodeURIComponent(  Pebble.getAccountToken() )  + '/' + encodeURIComponent(workoutName) + "/none"; 
+      postRequest(url);
   }
   
-
 }
 
-function postWorkoutCompleted(url){ 
+/**
+* Makes a POST request to given URL
+* @param String: url: URL to make request to 
+*/
+function postRequest(url){ 
   //Send request to server
   console.log(url);
   var req = new XMLHttpRequest();
@@ -150,23 +183,25 @@ function postWorkoutCompleted(url){
   req.onload = function(e) {
     if (req.readyState == 4 && req.status == 200) {
       if(req.status == 200) {
-        console.log("Workout recorded"); 
+        console.log("POST Request went through"); 
       } else { console.log('Error'); }
     }
   }; 
   req.send(null);
-
 }
 
-//Recieve message from Pebble
-Pebble.addEventListener("appmessage",
-  function(e) {
-    console.log("Recieved something on phone---");
+/**
+* Parses messages received from Pebble. Called when phone receives a message
+* @param e: Event. (see Pebble API for details)
+*/
+function parsePebbleMessage(e) { 
+   console.log("Recieved something on phone---");
     for (var type in e.payload){
         console.log("Received message: " + e.payload[type] + " of type: " + type);
         if (type === 'WORKOUT') workoutMessageHandler(e.payload[type]); 
         if (type === 'WORKOUT_DONE') workoutDoneMessageHandler(e.payload[type]);           
     }
-  }
-);
+}
+
+Pebble.addEventListener("appmessage", parsePebbleMessage);
 
