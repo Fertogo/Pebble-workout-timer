e3c10c3da81a63ab04f09c613db7c5968e190ca2
David Moreau <dav.m85@gmail.com>
2016-06-17 23:39:06 -0400

    moving to multiple js implementation

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,6 @@ Copyright © 2016 Fernando Trujano
                  trujano@mit.edu
 */
 
-//--------- js-message-queue ----------//
-"use strict";(function(root,factory){if(typeof define==="function"&&define.amd){define([],factory)}else if(typeof module==="object"&&module.exports){module.exports=factory()}else{root.MessageQueue=factory()}})(this,function(){var RETRY_MAX=5;var queue=[];var sending=false;var timer=null;return{reset:reset,sendAppMessage:sendAppMessage,size:size};function reset(){queue=[];sending=false}function sendAppMessage(message,ack,nack){if(!isValidMessage(message)){return false}queue.push({message:message,ack:ack||null,nack:nack||null,attempts:0});setTimeout(function(){sendNextMessage()},1);return true}function size(){return queue.length}function isValidMessage(message){if(message!==Object(message)){return false}var keys=Object.keys(message);if(!keys.length){return false}for(var k=0;k<keys.length;k+=1){var validKey=/^[0-9a-zA-Z-_]*$/.test(keys[k]);if(!validKey){return false}var value=message[keys[k]];if(!validValue(value)){return false}}return true;function validValue(value){switch(typeof value){case"string":return true;case"number":return true;case"object":if(toString.call(value)==="[object Array]"){return true}}return false}}function sendNextMessage(){if(sending){return}var message=queue.shift();if(!message){return}message.attempts+=1;sending=true;Pebble.sendAppMessage(message.message,ack,nack);timer=setTimeout(function(){timeout()},1e3);function ack(){clearTimeout(timer);setTimeout(function(){sending=false;sendNextMessage()},200);if(message.ack){message.ack.apply(null,arguments)}}function nack(){clearTimeout(timer);if(message.attempts<RETRY_MAX){queue.unshift(message);setTimeout(function(){sending=false;sendNextMessage()},200*message.attempts)}else{if(message.nack){message.nack.apply(null,arguments)}}}function timeout(){setTimeout(function(){sending=false;sendNextMessage()},1e3);if(message.ack){message.ack.apply(null,arguments)}}}});
-
 var VERSION = "4.0";
 var MESSAGE_DELIMITER = "\t"; 
 var MESSAGE_DATA_ROOT = 2; 
@@ -25,6 +22,7 @@ var MESSAGE_CHUNK_LENGTH;
 var DEV_MODE = false; //Set to false before Shipping! 
 var BASE_URL = 'http://pebble.fernandotrujano.com';
 
+var MessageQueue = require('./message-queue');
 
 Pebble.addEventListener("ready", function(e){
   console.log("JS code running!"); 
@@ -214,8 +212,8 @@ function parsePebbleMessage(e) {
    console.log("Recieved something on phone---");
     for (var type in e.payload){
         console.log("Received message: " + e.payload[type] + " of type: " + type);
-        if (type === 'WORKOUT') workoutMessageHandler(e.payload[type]); 
-        if (type === 'WORKOUT_DONE') workoutDoneMessageHandler(e.payload[type]);           
+        if (type == 0 /*'WORKOUT'*/) workoutMessageHandler(e.payload[type]); 
+        if (type == 1 /*'WORKOUT_DONE'*/) workoutDoneMessageHandler(e.payload[type]);           
     }
 }
 
